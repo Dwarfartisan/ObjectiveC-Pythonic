2571c09fd9f89c1ee13b6ca50a1bca572062cf00
March Liu <march.liu@gmail.com>
2013-03-22 15:38:39 +0800

    from to step test success

--- Pythonic/Tests/RangeTests.m
+++ Pythonic/Tests/RangeTests.m
@@ -13,9 +13,32 @@
 
 -(void) testRangeTo {
     NSArray * array = [[NSArray alloc] initWithTo:5];
+    NSInteger count = array.count;
+    GHAssertEquals((NSInteger)5, count, @"array should has 5 items, but now it is %ld", count);
     for (NSInteger idx=0; idx<5; idx++) {
         NSNumber *value = [array objectAtIndex:idx];
         GHAssertEquals(idx, [value integerValue], @"%ld should equal %@", idx, value);
+    }
+}
+
+-(void) testRangeFromTo {
+    NSArray *array = [[NSArray alloc] initWithFrom:3 to:12];
+    NSInteger count = array.count;
+    GHAssertEquals((NSInteger)9, count, @"array should has 9 items, but now it is %ld", count);
+    for (NSInteger idx=0; idx<9; idx++) {
+        NSNumber *value = [array objectAtIndex:idx];
+        GHAssertEquals(idx+3, [value integerValue], @"%ld should equal %@", idx, value);
+    }
+}
+
+-(void) testRangeFromToStep {
+    NSArray *array = [[NSArray alloc] initWithFrom:2 to:20 step:2];
+    NSInteger count = array.count;
+    GHAssertEquals((NSInteger)9, count, @"array should has 9 items, but now it is %ld", count);
+    for (NSInteger idx=0; idx<9; idx++) {
+        NSNumber *value= [array objectAtIndex:idx];
+        NSInteger n = (idx+1)*2;
+        GHAssertEquals(n, [value integerValue], @"%ld should equal %@", n, value);
     }
 }
 
